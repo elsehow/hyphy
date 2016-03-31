fed2916e986086ba1c2b26b11a4ae7e087802a0b
elsehow <yes@cosmopol.is>
2016-03-30 17:19:57 -0700

    fixed again

--- index.js
+++ index.js
@@ -3,12 +3,5 @@ var Kefir = require('kefir')
 module.exports = (log) => {
   // hyperlog data cb is (err, cb)
   // so we can use that kefir fromStream on the log
-  return Kefir.stream(e => {
-    log.createReadStream({live: true}).on('data', (err, node) => {
-      if (err) 
-        e.error(err)
-      if (node) 
-        e.emit(node)
-    })
-  })
+  return Kefir.fromEvents(log.createReadStream({live: true}), 'data')
 }
