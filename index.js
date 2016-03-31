5f2405eadf1f9930ca2f5fb90e1a5b76f9fc2768
elsehow <yes@cosmopol.is>
2016-03-30 17:12:41 -0700

    fixed bug where stream ended prematurely

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var Kefir = require('kefir')
 module.exports = (log) => {
   // hyperlog data cb is (err, cb)
   // so we can use that kefir fromStream on the log
-  return Kefir.fromCallback(cb => {
-    log.createReadStream({live: true}).on('data', cb)
+  return Kefir.stream(e => {
+    log.createReadStream({live: true}).on('data', (err, node) => {
+      if (err) 
+        e.error(err)
+      if (node) 
+        e.emit(node)
+    })
   })
 }
